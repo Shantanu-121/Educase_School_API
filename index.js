bbea9a5899719ff922aa77a38f345ac0aec7348d
Shantanu-121 <143492642+Shantanu-121@users.noreply.github.com>
2025-05-25 19:01:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
